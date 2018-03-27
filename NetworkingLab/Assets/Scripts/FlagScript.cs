d17bb5bdea3d0366af76d5807a03f098eaef5767
Sean Duffy <sean.the.duffy@gmail.com>
2018-03-27 09:56:59 -0400

    Secondary Commit

---
+++ NetworkingLab/Assets/Scripts/FlagScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagScript : MonoBehaviour 
+{
+
+
+	void Start () 
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			Debug.Log ("Grabbed flag!");
+			this.transform.parent.transform.parent = col.gameObject.transform;
+			this.transform.parent.transform.localPosition -= new Vector3 (-0.25f, -1.5f, 0.55f);
+			this.transform.parent.transform.localRotation = Quaternion.Euler(new Vector3(0.25f, 0.0f, 0.0f));
+			this.transform.parent.transform.localScale *= 0.5f;
+		}
+	}
+}
---
+++ NetworkingLab/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour 
+{
+	public float linearSpeed = 5.0f;
+	public float angularSpeed = 5.0f;
+
+	private Rigidbody rb = null;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float rotationalInput = Input.GetAxis ("Horizontal");
+		float forwardInput = Input.GetAxis ("Vertical");
+
+		Vector3 forwardVector = this.transform.forward;
+		Vector3 linearVelocity = this.transform.forward * (forwardInput * linearSpeed);
+		rb.velocity = linearVelocity;
+
+		Vector3 angularVelocity = this.transform.up * (rotationalInput * angularSpeed);
+		rb.angularVelocity = angularVelocity;
+	}
+}
